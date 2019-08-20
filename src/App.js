61134bca0cfc9b4bd6f82aadcc44b467cb100017
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-08-20 15:42:52 +0700

    Profile reducer and dashboard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
@@ -31,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/login' component={Login} />
               <Route exact path='/register' component={Register} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
